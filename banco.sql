8907172be1f692547e32622cec0f8424b351c2d3
jessicaertel <jessica.ertel@hotmail.com>
2024-11-23 18:06:56 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,8 +1,27 @@
-CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
-    Host varchar(50)
+CREATE TABLE disciplinas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE assuntos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    disciplina_id INT,
+    FOREIGN KEY (disciplina_id) REFERENCES disciplinas(id)
+);
+
+CREATE TABLE questoes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    enunciado TEXT NOT NULL,
+    assunto_id INT,
+    nivel_dificuldade ENUM('Fácil', 'Médio', 'Difícil'),
+    FOREIGN KEY (assunto_id) REFERENCES assuntos(id)
+);
+
+CREATE TABLE alternativas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    texto TEXT NOT NULL,
+    correta BOOLEAN,
+    questao_id INT,
+    FOREIGN KEY (questao_id) REFERENCES questoes(id)
 );
